ee0a9e314c02bd9d6c19e8c1792e8de47995988f
Tony-Kara <eniolaanthony@gmail.com>
2021-10-29 01:17:35 +0600

    - Add AVkit as a framework - Hosted video on Github - Add swift file to use in accessing GitHub page link - Add text description and text information - Add video

---
+++ VideoAppChallenge/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  VideoAppChallenge
+//
+//  Created by mac on 10/29/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var videohostUrl = "https://tony-kara.github.io/VideosHost/"
+}
--- VideoAppChallenge/ContentView.swift
+++ VideoAppChallenge/ContentView.swift
@@ -6,11 +6,30 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        let url = URL(string: Constants.videohostUrl + "upload.mp4")
+        
+        VStack(alignment: .leading, spacing: 10.0) {
+            
+            if let urlString = url {
+                VideoPlayer(player: AVPlayer(url: urlString))
+                    .cornerRadius(10)
+                    .padding()
+            }
+            
+            Text("Description")
+                .font(.title)
+                .padding(.leading)
+                
+            
+            Text("Labels and button has intrinsic values, so height and width is dertermined by the text font and size, ensure that you maintain the same height and fony size for all your buttons and labels, UIVIEW is not intrinsic in nature, so the constraits can be dertermined by the subviews present in it, if there are no subviews when it is created, you need to add both height and width. Do not add height for imageview as it changes with the phone screen size")
+                .padding()
+            
+        }
     }
 }
 
