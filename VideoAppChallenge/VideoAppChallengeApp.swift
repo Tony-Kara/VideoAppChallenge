43d74361b486424e793180868e41c9e617e8bc69
Tony-Kara <eniolaanthony@gmail.com>
2021-10-29 00:34:29 +0600

    Initial Commit

---
+++ VideoAppChallenge/VideoAppChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoAppChallengeApp.swift
+//  VideoAppChallenge
+//
+//  Created by mac on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct VideoAppChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
